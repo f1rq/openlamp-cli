56bdba3738da1b55f9322e59957f83f5d2714e20
Fabio <109223773+f1rq@users.noreply.github.com>
2025-12-28 15:44:51 +0100

    feat: add brightness control (#1)

--- cmd/openlamp-cli/main.go
+++ cmd/openlamp-cli/main.go
@@ -4,15 +4,30 @@ import (
 	"fmt"
 	"openlamp-cli/core"
 	"os"
+	"strconv"
 )
 
 func main() {
 	if len(os.Args) < 2 {
-		fmt.Println("usage: openlamp-cli <command>")
+		fmt.Println("usage: openlamp-cli <command> [value]")
 		return
 	}
 
 	var err error
+
+	command := os.Args[1]
+	if command == "turnon" || command == "turnoff" {
+		if len(os.Args) != 2 {
+			fmt.Println("Incorrect number of arguments")
+			return
+		}
+	}
+	if command == "brightness" {
+		if len(os.Args) != 3 {
+			fmt.Println("Please specify brightness value (0-255)")
+			return
+		}
+	}
 
 	err = core.ConnectToLamp()
 	if err != nil {
@@ -20,11 +35,18 @@ func main() {
 		os.Exit(1)
 	}
 
-	switch os.Args[1] {
+	switch command {
 	case "turnon":
 		err = core.TurnOn()
 	case "turnoff":
 		err = core.TurnOff()
+	case "brightness":
+		value, err := strconv.Atoi(os.Args[2])
+		if err != nil || value < 0 || value > 255 {
+			fmt.Println("Please specify brightness value (0-255)")
+			return
+		}
+		err = core.SetBrightness(byte(value))
 	default:
 		fmt.Println("unknown command:", os.Args[1])
 		os.Exit(1)
--- core/lamp.go
+++ core/lamp.go
@@ -2,6 +2,7 @@ package core
 
 var CmdTurnOn = []byte{0x55, 0xAA, 0x01, 0x08, 0x05, 0x01, 0xF1}
 var CmdTurnOff = []byte{0x55, 0xAA, 0x01, 0x08, 0x05, 0x00, 0xF2}
+var CmdBrightness = []byte{0x55, 0xAA, 0x01, 0x08, 0x01} // This byte array does not contain brightness value and checksum
 
 func TurnOn() error {
 	return WriteToLamp(CmdTurnOn)
@@ -9,4 +10,26 @@ func TurnOn() error {
 
 func TurnOff() error {
 	return WriteToLamp(CmdTurnOff)
+}
+
+func SetBrightness(value byte) error {
+	if value > 255 {
+		value = 255
+	}
+
+	cmd := make([]byte, len(CmdBrightness))
+	copy(cmd, CmdBrightness)
+
+	cmd = append(cmd, value)
+	cmd = append(cmd, ComputeChecksum(cmd))
+
+	return WriteToLamp(cmd)
+}
+
+func ComputeChecksum(cmd []byte) byte {
+	sum := 0
+	for _, b := range cmd {
+		sum += int(b)
+	}
+	return byte(0xFF - (sum & 0xFF))
 }
