4699da430d576331d8586efcde12862d272a9a8f
Fabio <109223773+f1rq@users.noreply.github.com>
2025-12-28 17:35:21 +0100

    feat: add color control (#2)

--- cmd/openlamp-cli/main.go
+++ cmd/openlamp-cli/main.go
@@ -22,11 +22,13 @@ func main() {
 			return
 		}
 	}
-	if command == "brightness" {
-		if len(os.Args) != 3 {
-			fmt.Println("Please specify brightness value (0-255)")
-			return
-		}
+	if command == "brightness" && len(os.Args) != 3 {
+		fmt.Println("Please specify brightness value (0-255)")
+		return
+	}
+	if command == "color" && len(os.Args) != 3 {
+		fmt.Println("Please specify hex color (without #)")
+		return
 	}
 
 	err = core.ConnectToLamp()
@@ -47,6 +49,21 @@ func main() {
 			return
 		}
 		err = core.SetBrightness(byte(value))
+	case "color":
+		hexColor := os.Args[2]
+
+		if len(hexColor) != 6 {
+			fmt.Println("Please specify a 6-character hex color (without #)")
+			return
+		}
+
+		_, err := strconv.ParseUint(hexColor, 16, 32)
+		if err != nil {
+			fmt.Println("Invalid hex color. Use only characters 0-9 and a-f")
+			return
+		}
+
+		err = core.SetColor(hexColor)
 	default:
 		fmt.Println("unknown command:", os.Args[1])
 		os.Exit(1)
